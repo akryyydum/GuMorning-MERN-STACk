46ef81aeeb615ec930853906be1e8c13f5670730
ACER <ACER@LAPTOP-B2BO8E6M>
2025-05-30 20:13:20 +0800

    fix

--- server/index.js
+++ server/index.js
@@ -14,7 +14,6 @@ app.use(express.json());
 
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.listen(5000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
 // Connect to MongoDB Atlas
